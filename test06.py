8c9d06b11675b5a6c41ef7603f5408238353930e
6652410005ChanachonPimpun <s6652410005@sau.ac.th>
2025-08-22 10:27:46 +0700

    test8work

---
+++ test06.py
@@ -0,0 +1,32 @@
+# operator คือ ตัวดำเนินการ คือเครื่องหมายของการประมวลผลข้อมูล
+# 1 arithmetic operator เครื่องหมายคำนวณ
+# (+,-,*,/,% mod,// floor,** eponential)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3) 
+print(10 / 3)
+print(10 % 3) #หารเอาเศษ
+print(10 // 3) #หารเอาส่วน
+print(10 ** 3)
+print('---------------------')
+# 2 comparison operator เครื่องหมายเปรียมเทียบ (==,!=,>,<,>=,<=)
+# ผลลัพธืมีแค่ True/False
+print(10 == 3)
+print(10 != 3)
+print('bangna' >= 'bangpoo')
+print('thailand' < 'THAILAND')
+print('---------------------')
+# 3 logical operator เครื่องหมายตรรกะ (not,and,or)
+# ข้อมูลที่จะใช้กับเครื่องหมายต้องเป็น True/False และผลลัพธ์ต้องเป็น True/False 
+print(not True)
+print(not False)
+print('-----')
+print(True and True) # True
+print(True and False)
+print(False and True)
+print(False and False)
+print('-----')
+print(True or True) # False
+print(True or False)
+print(False or True)
+print(False or False)
