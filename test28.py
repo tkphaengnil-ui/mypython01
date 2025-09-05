640591e060af9afdae102c20e3d4d78ddddf5d5d
thanawut kamphaengnil <tkphaengnil@gmail.com>
2025-09-05 11:59:52 +0700

    testwork30

---
+++ test28.py
@@ -0,0 +1,11 @@
+#Function คือ การทำงานหนึ่งๆ จะไม่ทำงานหากไม่เรียกใช้ (call function)
+#Function มีที่มาที่ไปยังไงบ้าง
+# - standard Function/ bulid-in function มากับภาษาโปรแกรม
+# - user define function ฟังก์ชันที่ dev สร้างขึ้นเอง
+# - Third-party function ฟังก์ชันที่คนอื่นสร้างขึ้น
+
+# Function มี 4 ประเภท
+# no parameter no return
+# have parameter no return
+# no parameter have return
+# have parameter have return
---
+++ test30.py
@@ -0,0 +1,18 @@
+#have parameter no return
+'''
+def func_name(param1, param2):
+    คำสั่ง
+    คำสั่ง
+    คำสั่ง
+    ....
+'''
+def showHEY(your_name):
+    print('สวัสดี',your_name,'!')
+
+def sumnumber(num1,num2):
+    print('ผลบวกของ',num1,'และ',num2,'คือ',num1+num2)
+
+showHEY('sad') #ข้อมูลที่ส่งให้ parameters เรียก argument
+showHEY('sdf')
+sumnumber(10,20)
+showHEY('aasfg')
